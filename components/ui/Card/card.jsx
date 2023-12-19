5084716149aad40b72630f91c4d4b54e7d2f3ad5
xDeFc0nx <nehartale8@gmail.com>
2023-12-19 20:24:37 +0100

    style: bg white and some other css changes

--- components/ui/Card/card.jsx
+++ components/ui/Card/card.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function card({ icon, header, value }) {
   return (
-    <Card className="w-[20rem] h-[13rem]  text-white  bg-secondary-gray/20  backdrop-filter backdrop-blur-lg	   shadow-lg">
+    <Card className="w-[20rem] h-[13rem]  text-white px-2 py-2  bg-white/10  backdrop-filter backdrop-blur-lg	   shadow-lg">
       <CardHeader className="w-full  justify-start">
         <Image height={30} width={30} radius="sm" src={icon} />
 
@@ -20,5 +20,25 @@ export default function card({ icon, header, value }) {
         <p className="text-sm text-[#92929B]">{value}</p>
       </CardBody>
     </Card>
+  );
+}
+
+export function CardDashboard({ icon, header, value, color }) {
+  return (
+    <div
+      className={` w-[30rem]  h-[13rem] p-5 text-white  bg-secondary-gray/50  backdrop-filter backdrop-blur-lg	  rounded-lg  shadow-lg`}
+    >
+      <div className="w-full flex justify-start">
+        <Image height={20} width={20} radius="sm" src={icon} />
+
+        <div className="flex flex-col justify-center">
+          <p className="text-lg  text-[#92929B] pl-1">{header}</p>
+        </div>
+      </div>
+
+      <div className="pt-4">
+        <p className={`text-4xl ${color}  `}>{value}</p>
+      </div>
+    </div>
   );
 }
