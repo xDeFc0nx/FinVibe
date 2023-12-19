62f9ed995bb4ad24e76efb9b0188bdd3eb6d71dc
xDeFc0nx <nehartale8@gmail.com>
2023-12-19 20:26:34 +0100

    style: updated styles for illustration

--- components/ui/LandingPage/hero.jsx
+++ components/ui/LandingPage/hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 import Illustration from "./illustration";
 import Github from "../../Icon/github.svg";
 
@@ -19,40 +20,48 @@ const hero = () => (
         Empower your finances with FinVibe's smart tools and expert guidance
       </p>
       <div className="space-x-5 flex">
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          type="button"
-          className="bg-primary-blue   justify-center gap-1 w-32 h-10 font-bold p-2 rounded-full text-white  "
-        >
-          Get Started{" "}
-          <svg
-            className="inline"
-            width="20"
-            height="10"
-            viewBox="0 0 20 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <Link href="/dashboard">
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            type="button"
+            className="bg-primary-blue   flex items-center  space-x-2 w-40 h-10 font-bold px-4 py-2  rounded-full text-white  "
           >
-            <path
-              d="M15.0125 3.75H0V6.25H15.0125V10L20 5L15.0125 0V3.75Z"
-              fill="white"
-            />
-          </svg>
-        </motion.button>
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          type="button"
-          className=" flex justify-center gap-1 border border-[#3F3F46] font-bold p-2 w-32 h-10 rounded-full text-white "
-        >
-          <Image src={Github} width={20} height={20} />
-          Github
-        </motion.button>
+            <span>Get Started</span>
+            <svg
+              className="inline"
+              width="20"
+              height="10"
+              viewBox="0 0 20 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15.0125 3.75H0V6.25H15.0125V10L20 5L15.0125 0V3.75Z"
+                fill="white"
+              />
+            </svg>
+          </motion.button>
+        </Link>
+        <Link href="https://github.com/xDeFc0nx/FinVibe">
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            type="button"
+            className=" flex items-center  border border-[#3F3F46]  justify-center gap-1 w-36 h-10 font-bold px-4 py-2 rounded-full text-white "
+          >
+            <Image src={Github} width={20} height={20} />
+            Github
+          </motion.button>
+        </Link>
       </div>
     </div>
-    <div className="pl-56">
-      <Illustration />
+    <div className="pl-56 pt-20 flex items-center ">
+      <div
+        className={` flex text-white w-[30rem] h-[20rem] bg-white/10  backdrop-filter backdrop-blur-md rounded-lg 	   shadow-lg`}
+      >
+        <Illustration />
+      </div>
     </div>
   </div>
 );
