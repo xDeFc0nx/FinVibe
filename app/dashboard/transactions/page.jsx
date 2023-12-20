1ca35aacf7db1613a7d56066cf5da21d11fdc107
xDeFc0nx <nehartale8@gmail.com>
2023-12-20 17:34:31 +0100

    fix(#1): simple fix

--- app/dashboard/transactions/page.jsx
+++ app/dashboard/transactions/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import Link from "next/link";
 import Navbar from "../../../components/ui/Navbar";
 import Table from "../../../components/ui/Table";
-import NewBtn from "../../../components/ui/Table/newTransaction";
+import Button from "../../../components/ui/Button";
 
 function page() {
   return (
@@ -17,7 +18,9 @@ function page() {
       {/* Main Content */}
       <div className="flex flex-col flex-1 p-20">
         <div className="grid grid-cols-1 gap-5 pt-10">
-          <NewBtn />
+          <Link href="/dashboard/new">
+            <Button Type="button" color="bg-green-500" Text="New" />
+          </Link>
           <Table />
         </div>
       </div>
