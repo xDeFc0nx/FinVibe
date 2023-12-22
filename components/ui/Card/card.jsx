52b11b9af4bffb7fe68779eaf898c6fd7d25a6b9
xDeFc0nx <nehartale8@gmail.com>
2023-12-22 23:45:35 +0100

    fix: removed nextui

--- components/ui/Card/card.jsx
+++ components/ui/Card/card.jsx
@@ -2,24 +2,23 @@
 /* eslint no-underscore-dangle: 0 */
 
 import React from "react";
-import { Card, CardHeader, CardBody } from "@nextui-org/card";
 import Image from "next/image";
 
 export default function card({ icon, header, value }) {
   return (
-    <Card className="w-[20rem] h-[13rem]  text-white px-2 py-2  bg-white/10  backdrop-filter backdrop-blur-lg	 shadow-lg">
-      <CardHeader className="w-full  justify-start">
+    <div className="w-[20rem] h-[13rem]  text-white px-4 py-4 rounded-md bg-white/10  backdrop-filter backdrop-blur-lg	 shadow-lg">
+      <div className="w-full flex justify-start">
         <Image height={30} width={30} radius="sm" src={icon} />
 
         <div className="flex flex-col justify-center">
           <p className="text-lg  text-white pl-1">{header}</p>
         </div>
-      </CardHeader>
+      </div>
 
-      <CardBody>
+      <div>
         <p className="text-sm text-[#92929B]">{value}</p>
-      </CardBody>
-    </Card>
+      </div>
+    </div>
   );
 }
 
