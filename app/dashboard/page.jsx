a41ceaaf0526f7ea6e8ceaac535a294e4ce57fdf
xDeFc0nx <nehartale8@gmail.com>
2023-12-23 20:33:21 +0100

    fix: clean up

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import Navbar from "../../components/ui/Navbar";
-import Card from "../../components/ui/Card/DashboardCards";
+import Card from "../../components/ui/DashboardPage/DashboardCards";
+import Topnav from "../../components/ui/DashboardPage/topnav";
+import { Chart } from "../../components/ui/DashboardPage/chart";
 
 const IndexPage = () => (
   <div className="flex h-screen">
@@ -14,12 +16,11 @@ const IndexPage = () => (
 
     {/* Main Content */}
     <div className="flex flex-col flex-1 p-20">
+      <Topnav />
       <Card />
 
-      <div className="grid grid-cols-1 gap-5 pt-10">
-        {/* <div className="w-full h-full  bg-secondary-gray/50  backdrop-filter backdrop-blur-lg	    shadow-lg">
-          test
-        </div> */}
+      <div className="grid grid-cols-1  gap-5 pt-10">
+        <Chart />
       </div>
     </div>
   </div>
