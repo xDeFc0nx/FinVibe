d04a785c840c52d153fd6f768e055f3313ccb838
xDeFc0nx <nehartale8@gmail.com>
2024-12-25 11:05:51 +0100

    feat: updateTransaction handler

--- handlers/Transactions.go
+++ handlers/Transactions.go
@@ -20,12 +20,12 @@ func CreateTransaction(conn *websocket.Conn, data json.RawMessage, userID string
 	transaction.UserID = userID
 
 	if err := json.Unmarshal(data, &transaction); err != nil {
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"Invalid transaction data"}`))
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Invalid transaction data"}`))
 		return
 	}
 
 	if err := db.DB.Create(&transaction).Error; err != nil {
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"Failed to create transaction"}`))
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Failed to create transaction"}`))
 		return
 	}
 
@@ -37,8 +37,8 @@ func GetTransactions(conn *websocket.Conn, userID string) {
 	transactions := []types.Transaction{}
 
 	if err := db.DB.Where("user_id = ?", userID).Find(&transactions).Error; err != nil {
-		fmt.Printf("Database error: %v\n", err)
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"Failed to get transactions"}`))
+		fmt.Printf("Database Error: %v\n", err)
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Failed to get transactions"}`))
 		return
 	}
 
@@ -52,19 +52,42 @@ func GetTransactionById(conn *websocket.Conn, data json.RawMessage, userID strin
 	transaction.UserID = userID
 
 	if err := json.Unmarshal(data, &transaction); err != nil {
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"Invalid transaction data"}`))
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Invalid transaction data"}`))
 		return
 	}
 	if transaction.UserID != userID {
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"Transaction does not belong to the user"}`))
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Transaction does not belong to the user"}`))
 		return
 	}
 
 	if err := db.DB.Where("id = ?", transaction.ID).First(&transaction).Error; err != nil {
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"error":"Transaction not found"}`))
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Transaction not found"}`))
 		return
 	}
 
 	response, _ := json.Marshal(transaction)
 	conn.WriteMessage(websocket.TextMessage, response)
+}
+
+func UpdateTransction(conn *websocket.Conn, data json.RawMessage, userID string) {
+	transaction := new(types.Transaction)
+
+	if err := json.Unmarshal(data, &transaction); err != nil {
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error": "Invalid Data"}`))
+	}
+
+	if transaction.UserID != userID {
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Transaction does not belong to the user"}`))
+		return
+	}
+
+	if err := db.DB.Where("id = ?", transaction.ID).First(&transaction).Error; err != nil {
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Transaction not found"}`))
+		return
+	}
+
+	if err := db.DB.Save(transaction).Error; err != nil {
+		conn.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Failed to Update"}`))
+	}
+	conn.WriteMessage(websocket.TextMessage, []byte(`{"Success":"Transaction Updated"}`))
 }
