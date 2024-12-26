8e279ea90db50318fac20426fede8834dddb52db
xDeFc0nx <nehartale8@gmail.com>
2024-12-26 15:14:34 +0100

    feat: transaction by account id and accountBalance

--- handlers/Transactions.go
+++ handlers/Transactions.go
@@ -18,6 +18,7 @@ func CreateTransaction(c *websocket.Conn, data json.RawMessage, userID string) {
 	transaction.ID = uuid.NewString()
 
 	transaction.UserID = userID
+	transaction.AccountID = account.ID
 
 	if err := json.Unmarshal(data, &transaction); err != nil {
 		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Invalid transaction data"}`+err.Error())); err != nil {
@@ -26,7 +27,14 @@ func CreateTransaction(c *websocket.Conn, data json.RawMessage, userID string) {
 		return
 	}
 
-	if err := db.DB.Where("user_id =? AND account_id =?", userID, account.ID).First(&account).Error; err != nil {
+	if transaction.AccountID == "" {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Account ID is required"}`)); err != nil {
+			logger.Error("%s", err.Error())
+		}
+		return
+	}
+
+	if err := db.DB.Where("user_id =? AND id =?", userID, transaction.AccountID).First(&account).Error; err != nil {
 		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Account not found"}`+err.Error())); err != nil {
 			logger.Error("%s", err.Error())
 		}
@@ -39,33 +47,75 @@ func CreateTransaction(c *websocket.Conn, data json.RawMessage, userID string) {
 		return
 	}
 
-	response, _ := json.Marshal(transaction)
-	if err := c.WriteMessage(websocket.TextMessage, response); err != nil {
+	response := map[string]interface{}{
+		"Success": "Created Transaction",
+		"Transaction": map[string]string{
+			"ID":        transaction.ID,
+			"UserID":    transaction.UserID,
+			"AccountID": transaction.AccountID,
+		},
+	}
+
+	responseData, _ := json.Marshal(response)
+
+	if err := c.WriteMessage(websocket.TextMessage, responseData); err != nil {
 		logger.Error("%s", err.Error())
 	}
 
 }
 func GetTransactions(c *websocket.Conn, data json.RawMessage, userID string) {
-	transactions := []types.Transaction{}
 	account := new(types.Accounts)
 
-	if err := json.Unmarshal(data, &transactions); err != nil {
-		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Invalid transaction data"}`)); err != nil {
+	// Assuming the data contains a single object (AccountID)
+	var requestData struct {
+		AccountID string `json:"AccountID"`
+	}
+
+	if err := json.Unmarshal(data, &requestData); err != nil {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Invalid request data"}`+err.Error())); err != nil {
 			logger.Error("%s", err.Error())
 		}
 		return
 	}
-	if err := db.DB.Where("user_id =? AND account_id =?", userID, account.ID).First(&account).Error; err != nil {
+
+	if err := db.DB.Where("user_id =? AND id =?", userID, requestData.AccountID).First(&account).Error; err != nil {
 		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Account not found"}`)); err != nil {
 			logger.Error("%s", err.Error())
 		}
 		return
 	}
 
-	response, _ := json.Marshal(transactions)
-	if err := c.WriteMessage(websocket.TextMessage, response); err != nil {
+	transactions := []types.Transaction{}
+	if err := db.DB.Where("account_id = ?", requestData.AccountID).Find(&transactions).Error; err != nil {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Could not fetch transactions"}`)); err != nil {
+			logger.Error("%s", err.Error())
+		}
+		return
+	}
+
+	transactionData := make([]map[string]interface{}, len(transactions))
+
+	for i, t := range transactions {
+		transactionData[i] = map[string]interface{}{
+			"ID":        t.ID,
+			"UserID":    t.UserID,
+			"AccountID": t.AccountID,
+			"Amount":    t.Amount,
+		}
+	}
+
+	// Package the response
+	response := map[string]interface{}{
+		"Success":      "Fetched Transactions",
+		"Transactions": transactionData,
+	}
+
+	// Marshal the response and send it
+	responseData, _ := json.Marshal(response)
+	if err := c.WriteMessage(websocket.TextMessage, responseData); err != nil {
 		logger.Error("%s", err.Error())
 	}
+
 }
 
 func GetTransactionById(c *websocket.Conn, data json.RawMessage, userID string) {
@@ -169,4 +219,43 @@ func DeleteTransaction(c *websocket.Conn, data json.RawMessage, userID string) {
 		logger.Error("%s", err.Error())
 	}
 
+}
+
+func GetAccountBalance(c *websocket.Conn, accountID string) error {
+	transactions := []types.Transaction{}
+	account := new(types.Accounts)
+
+	account.ID = accountID
+
+	if err := db.DB.Where(" id =?", account.ID).First(&account).Error; err != nil {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Account not found"}`+err.Error())); err != nil {
+			logger.Error("%s", err.Error())
+		}
+		return err
+	}
+
+	if err := db.DB.Where("account_id = ?", account.ID).Find(&transactions).Error; err != nil {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"Could not fetch transactions"}`)); err != nil {
+			logger.Error("%s", err.Error())
+		}
+		return err
+	}
+
+	totalBalance := float64(0)
+	for _, t := range transactions {
+		if t.AccountID == accountID {
+			totalBalance += t.Amount
+		}
+	}
+
+	account.Balance = totalBalance
+
+	if err := db.DB.Save(account).Error; err != nil {
+		if err := c.WriteMessage(websocket.TextMessage, []byte(`{"Error":"failed to Save"}`+err.Error())); err != nil {
+			logger.Error("%s", err.Error())
+		}
+		return err
+	}
+
+	return nil
 }
