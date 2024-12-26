0d99847811c022f57054b7bf9341e414458f05fb
xDeFc0nx <nehartale8@gmail.com>
2024-12-26 09:35:31 +0100

    feat: new types

--- types/types.go
+++ types/types.go
@@ -4,18 +4,6 @@ import (
 	"time"
 )
 
-type User struct {
-	ID                   string                `gorm:"primaryKey"`
-	FirstName            string                `json:"FirstName"`
-	LastName             string                `json:"lastName"`
-	Email                string                `gorm:"type:varchar(100);unique_index"`
-	Password             string                `json:"-"`
-	CreatedAt            time.Time             `json:"createdAt"`
-	UpdatedAt            time.Time             `json:"-"`
-	Transactions         []Transaction         `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	WebSocketConnections []WebSocketConnection `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-}
-
 type WebSocketConnection struct {
 	ID           string    `gorm:"primaryKey"`
 	ConnectionID string    `gorm:"connectionID"`
@@ -25,10 +13,35 @@ type WebSocketConnection struct {
 	CreatedAt    time.Time `json:"createdAt"`
 }
 
+type User struct {
+	ID                   string                `gorm:"primaryKey"`
+	FirstName            string                `json:"FirstName"`
+	LastName             string                `json:"lastName"`
+	Email                string                `gorm:"type:varchar(100);unique_index"`
+	Password             string                `json:"-"`
+	CreatedAt            time.Time             `json:"createdAt"`
+	UpdatedAt            time.Time             `json:"-"`
+	Accounts             []Accounts            `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	WebSocketConnections []WebSocketConnection `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type Accounts struct {
+	ID           string        `gorm:"primaryKey"`
+	UserID       string        `gorm:"not null"`
+	User         User          `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Type         string        `json:"type"`
+	Balance      float64       `json:"balance"`
+	Transactions []Transaction `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt    string        `json:"created"`
+	UpdatedAt    string        `json:"-"`
+}
+
 type Transaction struct {
 	ID          string    `gorm:"primaryKey"`
 	UserID      string    `gorm:"not null"`
 	User        User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	AccountID   string    `gorm:"not null"`
+	Account     Accounts  `gorm:"foreignKey:AccountID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Amount      float64   `json:"amount"`
 	Description string    `json:"description"`
 	IsRecurring bool      `gorm:"default:false"`
