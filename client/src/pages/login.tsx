94c774b629837bd8b0bf95084d336cdd9a491678
xDeFc0nx <nehartale8@gmail.com>
2025-01-04 04:31:32 +0100

    feat: login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
   Form,
   FormControl,
   FormDescription,
@@ -10,12 +17,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import * as z from "zod";
 
 const formSchema = z.object({
@@ -45,51 +52,100 @@ export default function MyForm() {
         document.cookie = `jwt=${responseData.token}; path=/; secure; httpOnly`;
         navigate("/app/dashboard");
       } else {
-        toast({ title: "Login failed", description: "Please try again." });
+        toast("Login Failed Try again");
       }
     } catch (error) {
-      toast({ title: "Login failed", description: "Please try again." });
+      toast("Login Failed Try again");
     }
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(handleSubmit)}
-        className="space-y-8 max-w-3xl mx-auto py-10"
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Email" type="email" {...field} />
-              </FormControl>
-              <FormDescription>Enter your email</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
+        <Link to="/register">
+          <Button className="absolute right-4 top-4 md:right-8 md:top-8">
+            Register
+          </Button>
+        </Link>
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="Password" type="password" {...field} />
-              </FormControl>
-              <FormDescription>Enter your password</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                {" "}
+                Welcome Back!
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Your data has been carefully wrapped in a secure layer of
+                encryption and stored in a digital vault,
+              </p>
+            </div>
 
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="grid w-full max-w-sm items-center gap-1.5"
+                          placeholder="Email"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>Enter your email</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="grid w-full max-w-sm items-center gap-1.5"
+                          placeholder="Password"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>Enter your password</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+            <p className="px-8 text-center text-sm text-muted-foreground pt-5">
+              By clicking continue, you agree to our{" "}
+              <a
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
