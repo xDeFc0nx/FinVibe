87b865bb5afdf4cf1158403e5c1efdbadcc34c7f
Nehar Tale <nehartale8@gmail.com>
2025-01-07 17:42:50 +0100

    chore: all writeMessage swiched to Message

--- handlers/Message.go
+++ handlers/Message.go
@@ -5,8 +5,8 @@ import (
 	"github.com/xDeFc0nx/logger-go-pkg"
 )
 
-func Message(ws *websocket.Conn, sendText string, err error) {
-	if err := ws.WriteMessage(websocket.TextMessage, []byte(sendText+err.Error())); err != nil {
+func Message(ws *websocket.Conn, sendText string) {
+	if err := ws.WriteMessage(websocket.TextMessage, []byte(sendText)); err != nil {
 		logger.Error("%s", err.Error())
 	}
 }
