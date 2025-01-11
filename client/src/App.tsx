603e8d27c992e5da7a292cf1bd8fdce11e1835b8
Nehar Tale <nehartale8@gmail.com>
2025-01-11 17:17:41 +0100

    fix: themechanger not needed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 
 import Hero from "@/components/ui/hero";
-import { ThemeChanger } from "./components/ui/theme";
 
 export default function App() {
     
   return (
     <>
       <Hero />
-      <ThemeChanger />
     </>
   );
 }
