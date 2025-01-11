62d9d0e3f3d316c1ace14671c5ce60a89ee7868b
Nehar Tale <nehartale8@gmail.com>
2025-01-11 17:15:06 +0100

    feat: Json Message

--- handlers/Message.go
+++ handlers/Message.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+
 	"github.com/gofiber/contrib/websocket"
 	"github.com/xDeFc0nx/logger-go-pkg"
 )
@@ -8,7 +10,8 @@ import (
 var InvalidData = "Error: Invalid form data"
 
 func Message(ws *websocket.Conn, sendText string) {
-	if err := ws.WriteMessage(websocket.TextMessage, []byte(sendText)); err != nil {
+	textJsonify, _ := json.Marshal(sendText)
+	if err := ws.WriteMessage(websocket.TextMessage, []byte(textJsonify)); err != nil {
 		logger.Error("%s", err.Error())
 	}
 }
