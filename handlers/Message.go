8121e49ee8fda37d52b011f9618c27c5d907a470
Nehar Tale <nehartale8@gmail.com>
2025-01-17 23:10:37 +0100

    "refactor(handlers/Message.go): split Message function into Send_Message and Send_Error"

--- handlers/Message.go
+++ handlers/Message.go
@@ -9,11 +9,27 @@ import (
 
 var InvalidData = "Error: Invalid form data"
 
-func Message(ws *websocket.Conn, sendText string, err error) {
-	textJsonify, _ := json.Marshal(sendText)
-	if err := ws.WriteMessage(websocket.TextMessage, []byte(textJsonify)); err != nil {
-		slog.Error("failed to send message", slog.String("error", err.Error()))
+func Send_Message(ws *websocket.Conn, sendText string) {
+	response := map[string]string{
+		"Message": sendText,
 	}
+	responseJSON, _ := json.Marshal(response)
+	_ = ws.WriteMessage(websocket.TextMessage, responseJSON)
+}
+
+func Send_Error(ws *websocket.Conn, sendText string, err error) {
+	response := map[string]interface{}{
+		"Message": sendText,
+	}
+
+	if err != nil {
+		response["Error"] = err.Error()
+	}
+
+	responseJSON, _ := json.Marshal(response)
+
+	_ = ws.WriteMessage(websocket.TextMessage, responseJSON)
+
 	if err != nil {
 		slog.Error(sendText, slog.String("error", err.Error()))
 	}
