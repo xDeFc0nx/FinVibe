d0f0ab3d2a84cc0f55396439357f3524531534d1
Nehar Tale <nehartale8@gmail.com>
2025-02-02 19:26:28 +0100

    chore: Remove actions column and update no results component

--- client/src/pages/(secure)/columns.tsx
+++ client/src/pages/(secure)/columns.tsx
@@ -45,32 +45,4 @@ export const columns: ColumnDef<Transaction>[] = [
 			);
 		},
 	},
-	{
-		id: "actions",
-		enableHiding: false,
-		cell: ({ row }) => {
-			const transaction = row.original;
-			return (
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="ghost" className="h-8 w-8 p-0">
-							<span className="sr-only">Open menu</span>
-							<MoreHorizontal />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>Actions</DropdownMenuLabel>
-						<DropdownMenuItem
-							onClick={() => navigator.clipboard.writeText(transaction.ID)}
-						>
-							Copy payment ID
-						</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>View customer</DropdownMenuItem>
-						<DropdownMenuItem>View payment details</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			);
-		},
-	},
 ];
