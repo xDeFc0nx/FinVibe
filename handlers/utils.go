e95f6100cae739ef935169cda15d6ed55181718a
Nehar Tale <nehartale8@gmail.com>
2025-04-09 20:25:26 +0200

    feat: Log stack traces on errors

--- handlers/utils.go
+++ handlers/utils.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
+	"fmt"
+	"github.com/pkg/errors"
+
 	"github.com/gofiber/contrib/websocket"
 )
 
@@ -22,20 +25,31 @@ func JSendSuccess(c *fiber.Ctx, data any) {
 		Status: "success",
 		Data:   data,
 	})
+
 }
 
-func JSendFail(c *fiber.Ctx, data any, code int) {
+func JSendFail(c *fiber.Ctx, data any, code int, err error) {
 	c.Status(code).JSON(Response{
 		Status: "fail",
 		Data:   data,
 	})
+	slog.Info("fail",
+		data,
+		slog.String("stack", fmt.Sprintf("%+v", errors.Wrap(err, ""))),
+	)
+
 }
 
-func JSendError(c *fiber.Ctx, data any, code int) {
+func JSendError(c *fiber.Ctx, data any, code int, err error) {
 	c.Status(code).JSON(Response{
 		Status: "error",
 		Data:   data,
 	})
+
+	slog.Error("error",
+		data,
+		slog.String("stack", fmt.Sprintf("%+v", errors.Wrap(err, ""))),
+	)
 }
 
 var InvalidData = "Error: Invalid form data"
@@ -47,12 +61,15 @@ func Send_Message(ws *websocket.Conn, sendText string) {
 }
 
 func Send_Error(ws *websocket.Conn, sendText string, err error) {
-	slog.Error("message", sendText, "error", err)
+	slog.Error("error",
+		slog.String("stack", fmt.Sprintf("%+v", errors.Wrap(err, ""))),
+	)
+
 	response := map[string]any{
 		"Error": sendText,
 	}
 	responseJSON, _ := json.Marshal(response)
 	if err := ws.WriteMessage(websocket.TextMessage, responseJSON); err != nil {
-		slog.Error(sendText, slog.String("error", err.Error()))
+		slog.Error(sendText, fmt.Sprintf("%+v", errors.Wrap(err, "")))
 	}
 }
