098c4af15c014d2930d7df2169f8040f5f6b3e62
Nehar Tale <nehartale8@gmail.com>
2025-04-16 00:16:19 +0200

    refactor: Replace useUserData with useSelector in columns

--- client/src/pages/(secure)/columns.tsx
+++ client/src/pages/(secure)/columns.tsx
@@ -1,50 +1,40 @@
 "use client";
 import type { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-import { useUserData, type Transaction } from "@/components/context/userData";
+import type { RootState, } from '@/store/store.ts';
+import type { Transaction, } from "@/types";
+import { useSelector, } from 'react-redux';
 export const columns: ColumnDef<Transaction>[] = [
-	{
-		accessorKey: "Description",
-		header: "Description",
-	},
-	{
-		accessorKey: "type",
-		header: () => <div className="text-right">Type</div>,
-		cell: ({ row }) => <div className="text-right">{row.original.Type}</div>,
-	},
+  {
+    accessorKey: "Description",
+    header: "Description",
+  },
+  {
+    accessorKey: "type",
+    header: () => <div className="text-right">Type</div>,
+    cell: ({ row }) => <div className="text-right">{row.original.Type}</div>,
+  },
 
-	{
-		accessorKey: "amount",
-		header: () => <div className="text-right">Amount</div>,
-		cell: ({ row }) => {
-			const amount = row.original.Amount;
-			const type = row.original.Type;
-			const { userData } = useUserData();
-			const formattedAmount =
-				type === "Income"
-					? `+${userData.Currency}${amount}`
-					: `-${userData.Currency}${amount}`;
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = row.original.Amount;
+      const type = row.original.Type;
+      const userData = useSelector((state: RootState) => state.user.data)
 
-			const backgroundColor =
-				type === "Income" ? "bg-green-400/50" : "bg-red-400/50";
+      const formattedAmount =
+        type === "Income"
+          ? `+${userData?.Currency}${amount}`
+          : `-${userData?.Currency}${amount}`;
 
-			return (
-				<div className={`text-right p-2 rounded-lg ${backgroundColor}`}>
-					{formattedAmount}
-				</div>
-			);
-		},
-	},
+      const backgroundColor =
+        type === "Income" ? "bg-green-400/50" : "bg-red-400/50";
+
+      return (
+        <div className={`text-right p-2 rounded-lg ${backgroundColor}`}>
+          {formattedAmount}
+        </div>
+      );
+    },
+  },
 ];
