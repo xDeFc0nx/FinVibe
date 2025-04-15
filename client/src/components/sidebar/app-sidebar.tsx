c3fb8796e521d5351244656afbb09f52d7e53675
Nehar Tale <nehartale8@gmail.com>
2025-04-15 16:51:56 +0200

    refactor: fetch sidebar user data from Redux

--- client/src/components/sidebar/app-sidebar.tsx
+++ client/src/components/sidebar/app-sidebar.tsx
@@ -3,46 +3,46 @@ import { LayoutDashboard, CreditCard } from "lucide-react";
 
 import { NavUser } from "@/components/sidebar/nav-user";
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarHeader,
-	SidebarRail,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
 } from "@/components/ui/sidebar";
-import { useUserData } from "@/components/context/userData";
 import { NavMain } from "@/components/sidebar/nav-main";
 import { AccountSwitcher } from "@/components/sidebar/account-switcher";
-
+import { useSelector } from 'react-redux';
+import type { RootState } from '@/store/store.ts';
 const data = {
-	Links: [
-		{
-			name: "Dashboard",
-			url: "/app/dashboard",
-			icon: LayoutDashboard,
-		},
-		{
-			name: "Transactions",
-			url: "/app/transactions",
-			icon: CreditCard,
-		},
-	],
+  Links: [
+    {
+      name: "Dashboard",
+      url: "/app/dashboard",
+      icon: LayoutDashboard,
+    },
+    {
+      name: "Transactions",
+      url: "/app/transactions",
+      icon: CreditCard,
+    },
+  ],
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	const { userData } = useUserData();
 
-	return (
-		<Sidebar collapsible="icon" {...props}>
-			<SidebarHeader>
-				<AccountSwitcher />
-			</SidebarHeader>
-			<SidebarContent>
-				<NavMain items={data.Links} />
-			</SidebarContent>
-			<SidebarFooter>
-				<NavUser user={userData} />
-			</SidebarFooter>
-			<SidebarRail />
-		</Sidebar>
-	);
+  const userData = useSelector((state: RootState) => state.user.data)
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <AccountSwitcher />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.Links} />
+      </SidebarContent>
+      <SidebarFooter>
+         {userData && <NavUser user={userData} />}   
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
